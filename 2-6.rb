e310a6c8f6a7127e23a3fe1b358e8549d7b0b5ec
Infratop <Infratop@infratopnoMacBook-Air.local>
2018-11-14 19:02:51 +0900

    [Finish]2-6

---
+++ 2-6.rb
@@ -0,0 +1,16 @@
+puts 100
+puts 100 + 5
+
+puts "Rubyを学習"
+name = "鈴木さん"
+puts "Hello" + name
+puts "こんにちは#{name}"
+
+names = ["Perl", "Ruby", "Python", "Swift"]
+puts names[1]
+
+tall = {"太郎"=>185, "二郎"=>170, "花子"=>150}
+puts tall["太郎"]
+
+tall = {:太郎=>185, :二郎=>170, :花子=>150}
+puts tall[:花子]
