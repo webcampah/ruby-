fd999997576358a0d1088c3891da1c58990ecf9d
Infratop <Infratop@infratopnoMacBook-Air.local>
2018-11-15 11:13:24 +0900

    [Finish]2-7

--- 2-7.rb
+++ 2-7.rb
@@ -7,3 +7,30 @@ end
 if apple <= 100
 	puts "りんごの値段は１００円以下です"
 end
+
+
+
+tall = 180
+
+if tall >= 170 && tall <= 190
+    puts "身長は170以上190以下です。"
+end
+
+
+apple = "Aomori"
+
+if apple == "Nagoya"
+	puts "りんごの生産地は長野です"
+else
+	puts "りんごの生産地は青森です"
+end
+
+apple = "Yamagata"
+
+if apple == "Nagano"
+	puts "りんごの生産地は長野です"
+elsif apple == "Yamagata"
+	puts "りんごの生産地は山形です"
+else
+	puts "りんごの生産地は青森です"
+end
